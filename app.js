cab8f993a02f16926c867858c4c4e6ec8956add6
Tamim Ehsan <tamimehsan99@gmail.com>
2021-02-01 10:55:58 +0600

    3 complete route => add, search, find

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
+const morgan = require('morgan');
+const cors=require('cors')
 
 const db = require("./config/database");
 
@@ -11,7 +13,11 @@ db.authenticate()
     .catch( err =>  console.log('Error: '+err) )
 
 const app = express();
-
+app.use(cors())
+app.options('*', cors());
+// for test purposes
+app.use(morgan('dev'));
+app.use(express.json());
 app.use('/gigs',require('./routes/gigs'));
 const PORT = process.env.PORT || 5000;
 
--- routes/gigs.js
+++ routes/gigs.js
@@ -1,13 +1,47 @@
 const express = require('express');
-const router = express.Router();
+const router = require('express-promise-router')();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
-router.get('/', (req,res) =>
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+router.get('/', (req, res) =>
     Gig.findAll()
-        .then( gigs => {
-            console.log(gigs);
-            res.sendStatus(200);
-        } )
+        .then(gigs => {
+            //console.log(gigs);
+            res.send({gigs});
+        })
         .catch(err => console.log(err)));
+
+router.route('/add')
+    .post( async (req, res, next) => {
+        const {title, technologies, budget, description, contact_email} = req.body;
+        // insert into table
+        try {
+            const gig = await Gig.create({
+                title,
+                technologies,
+                budget,
+                description,
+                contact_email
+            });
+            res.sendStatus(200);
+
+        } catch (err) {
+            res.send({err});
+        }
+
+    });
+
+router.route('/search')
+    .post( async (req,res) => {
+        const {term} = req.body.query;
+        try{
+            const gig = await Gig.findAll({ where: { technologies: { [Op.like] : '%' + term+ '%' }  }  })
+            res.send(gig);
+        }catch (e) {
+            res.send(e);
+        }
+    });
 
 module.exports = router;
